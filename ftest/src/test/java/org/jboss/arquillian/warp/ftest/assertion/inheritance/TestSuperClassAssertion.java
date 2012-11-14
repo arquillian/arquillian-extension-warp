ee8328f6b7e1ed60bf9c610571d05f63cf0bc90f
Lukas Fryc <lfryc@redhat.com>
2012-11-14 20:39:40 +0100

    ARQ-1196: added ftests for running assertions from super classes

---
+++ ftest/src/test/java/org/jboss/arquillian/warp/ftest/assertion/inheritance/TestSuperClassAssertion.java
@@ -0,0 +1,76 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.arquillian.warp.ftest.assertion.inheritance;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.net.URL;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.arquillian.warp.ClientAction;
+import org.jboss.arquillian.warp.Warp;
+import org.jboss.arquillian.warp.WarpTest;
+import org.jboss.arquillian.warp.ftest.TestingServlet;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * @author Lukas Fryc
+ */
+@RunWith(Arquillian.class)
+@WarpTest
+@RunAsClient
+public class TestSuperClassAssertion {
+
+    @Drone
+    WebDriver browser;
+
+    @ArquillianResource
+    URL contextPath;
+
+    @Deployment
+    public static WebArchive createDeployment() {
+
+        return ShrinkWrap.create(WebArchive.class, "test.war")
+                .addClasses(TestingServlet.class, AbstractAssertion.class)
+                .addAsWebResource(new File("src/main/webapp/index.html"))
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Test
+    public void test() {
+        AbstractAssertion assertion = Warp.execute(new ClientAction() {
+            public void action() {
+                browser.navigate().to(contextPath + "index.html");
+            }
+        }).verify(new AbstractAssertion() {
+            private static final long serialVersionUID = 1L;
+        });
+
+        assertTrue("before servlet must be run", assertion.isBeforeServletRun());
+        assertTrue("after servlet must be run", assertion.isAfterServletRun());
+    }
+}
