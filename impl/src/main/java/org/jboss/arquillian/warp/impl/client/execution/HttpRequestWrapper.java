8d5e200f2649466b0f1c06e507ccef07ee664492
Lukas Fryc <lfryc@redhat.com>
2014-01-15 08:49:22 +0100

    ARQ-1562: methods for retrieving query parameters and http-data from HttpRequest

--- impl/src/main/java/org/jboss/arquillian/warp/impl/client/execution/HttpRequestWrapper.java
+++ impl/src/main/java/org/jboss/arquillian/warp/impl/client/execution/HttpRequestWrapper.java
@@ -17,9 +17,20 @@
 package org.jboss.arquillian.warp.impl.client.execution;
 
 import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.QueryStringDecoder;
+import io.netty.handler.codec.http.multipart.Attribute;
+import io.netty.handler.codec.http.multipart.DefaultHttpDataFactory;
+import io.netty.handler.codec.http.multipart.HttpPostRequestDecoder;
+import io.netty.handler.codec.http.multipart.InterfaceHttpData;
+import io.netty.handler.codec.http.multipart.InterfaceHttpData.HttpDataType;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
@@ -28,7 +39,10 @@ import org.jboss.arquillian.warp.impl.utils.URLUtils;
 
 public class HttpRequestWrapper implements org.jboss.arquillian.warp.client.filter.http.HttpRequest {
 
-    private HttpRequest request;
+    private final HttpRequest request;
+
+    private Map<String, List<String>> queryParameters;
+    private Map<String, List<String>> httpDataAttributes;
 
     public HttpRequestWrapper(HttpRequest request) {
         this.request = request;
@@ -82,4 +96,54 @@ public class HttpRequestWrapper implements org.jboss.arquillian.warp.client.filt
     public String toString() {
         return String.format("%s %s", request.getMethod(), request.getUri());
     }
+
+    @Override
+    public Map<String, List<String>> getQueryParameters() {
+        if (queryParameters == null) {
+            QueryStringDecoder queryStringDecoder = new QueryStringDecoder(request.getUri());
+            queryParameters = queryStringDecoder.parameters();
+        }
+        return Collections.unmodifiableMap(queryParameters);
+    }
+
+    @Override
+    public Map<String, List<String>> getHttpDataAttributes() {
+
+        if (!HttpMethod.POST.equals(getMethod())) {
+            throw new IllegalArgumentException("Cannot parse HttpData for requests other than POST");
+        }
+
+        try {
+            if (httpDataAttributes == null) {
+                final HttpPostRequestDecoder decoder = new HttpPostRequestDecoder(new DefaultHttpDataFactory(false), request);
+                final Map<String, List<String>> map = new HashMap<String, List<String>>();
+
+                try {
+                    for (InterfaceHttpData data : decoder.getBodyHttpDatas()) {
+                        if (data.getHttpDataType() == HttpDataType.Attribute) {
+                            Attribute attribute = (Attribute) data;
+
+                            List<String> list = map.get(attribute.getName());
+                            if (list == null) {
+                                list = new LinkedList<String>();
+                                map.put(attribute.getName(), list);
+                            }
+
+                            list.add(attribute.getValue());
+                        }
+                    }
+                } finally {
+                    decoder.destroy();
+                }
+
+                httpDataAttributes = map;
+            }
+
+            return Collections.unmodifiableMap(httpDataAttributes);
+
+        } catch (IOException e) {
+            throw new IllegalStateException("Cannot parse http request data", e);
+        }
+    }
+
 }
